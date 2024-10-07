c418deaf2474b72a8879843ff16d9b568d8627bc
SonamTenzin7 <sonamtenzey0@gmail.com>
2024-10-07 23:44:14 +0600

    enent

---
+++ src/app/(DashboardLayout)/Events/calendar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "./EventCalendar";
+
+function Calendar() {
+    return (
+        <div>
+            <Fullcalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView={"dayGridMonth"}
+                headerToolbar={{
+                    start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+                    center: "title",
+                    end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+                }}
+                height={"90vh"}
+            />
+        </div>
+    );
+}
+
+export default Calendar;
--- src/app/(DashboardLayout)/Events/page.tsx
+++ src/app/(DashboardLayout)/Events/page.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+"use client";
 
-const Events = () => {
+import React from "react";
+import { Grid } from "@mui/material";
+import Calendar from "./calendar";
+import EventSidebar from "./eventsidebar";
+import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
+import DashboardCard from "@/app/(DashboardLayout)/components/shared/DashboardCard";
+
+const EventsPage: React.FC = () => {
   return (
-    <div>
-      <h1>Events Page</h1>
-      
-    </div>
+    <PageContainer title="Event Page" description="Members page">
+      <DashboardCard title="Event">
+        <Grid container spacing={3}>
+          {/* Sidebar Section (Left) */}
+          <Grid item xs={3}>
+            <EventSidebar />
+          </Grid>
+
+          {/* Calendar Section (Right) */}
+          <Grid item xs={9}>
+            <Calendar />
+          </Grid>
+        </Grid>
+      </DashboardCard>
+    </PageContainer>
   );
 };
 
-export default Events;
+export default EventsPage;
